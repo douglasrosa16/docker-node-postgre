fd945c79a0d779aa242e92340eeb786e57d54491
Douglas Oliveira <douglasoliveira163425@gmail.com>
2021-07-03 09:10:15 -0400

    ajustado controllers

--- src/controllers/ConsumerController.js
+++ src/controllers/ConsumerController.js
@@ -3,9 +3,27 @@ const Consumer = require('../models/Consumer');
 
 module.exports = {
   async index(req, res){
+    const { user_id } = req.params;
 
+    const user = await User.findByPk(user_id, {
+        include: { association: 'consumers' }
+    });
+    return res.json(user.consumers);
   },
-  async store(req, res){
 
+  async store(req, res){
+    const { user_id } = req.params;
+
+    const user = await User.findByPk(user_id);
+
+    if (!user) {
+        return res.status(400).json({error: 'Usuário não encontrado'});
+    }
+
+    const consumers = await Consumer.create({
+        user_id,
+    });
+
+    return res.json(consumers);
   }
 }
